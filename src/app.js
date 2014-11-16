3283da77f4f772175e10f248688b324db30f34ad
Alex <xzenfar@users.noreply.github.com>
2014-11-16 08:58:59 -0500

    Finished

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+// ------------------------------
+// Start of Strap API
+// ------------------------------
+var strap_api_url="https://api.straphq.com/create/visit/with/";var strap_api_clone=function(obj){if(null==obj||"object"!=typeof obj)return obj;var copy={};for(var attr in obj){if(obj.hasOwnProperty(attr))copy[attr]=obj[attr]}return copy};function strap_api_init_accel(accel,params){accel.config({rate:10,samples:10});var p=strap_api_clone(params);p["action_url"]="STRAP_API_ACCL";strap_api_accl_on(accel,p)}function strap_api_accl_on(accel,params){console.log("turning accl on");strap_api_log(params);accel.on("data",function(e){var now=(new Date).getTime();var base=0;if(e.accels.length>0){base=e.accels[0].time}for(var i=0;i<e.accels.length;i++){e.accels[i].time=now+e.accels[i].time-base}var tmpstore=window.localStorage["strap_accl"];if(tmpstore){tmpstore=JSON.parse(tmpstore)}else{tmpstore=[]}tmpstore=tmpstore.concat(e.accels);window.localStorage["strap_accl"]=JSON.stringify(tmpstore)});setTimeout(function(){strap_api_log(params);strap_api_accl_off(accel,params)},1e3*60*1)}function strap_api_accl_off(accel,params){console.log("turning accl off");strap_api_log(params);accel.off();setTimeout(function(){strap_api_log(params);strap_api_accl_on(accel,params)},1e3*60*2)}function strap_api_init(params){var lp=strap_api_clone(params);lp["action_url"]="STRAP_START";strap_api_log(lp)}function strap_api_conv_accel(data,act){var da=[];for(var i=0;i<data.length;i++){var d={};d.x=data[i].x;d.y=data[i].y;d.z=data[i].z;d.ts=data[i].time;d.vib=data[i].vibe?true:false;d.act=act;da.push(d)}return da}function strap_api_log(params){var lp=params;var req=new XMLHttpRequest;req.open("POST",strap_api_url,true);var tz_offset=(new Date).getTimezoneOffset()/60*-1;var query="app_id="+lp["app_id"]+"&resolution="+(lp["resolution"]||"")+"&useragent="+(lp["useragent"]||"")+"&action_url="+(lp["action_url"]||"")+"&visitor_id="+(lp["visitor_id"]||Pebble.getAccountToken())+"&act="+(lp["act"]||"")+"&visitor_timeoffset="+tz_offset;if(lp["action_url"]==="STRAP_API_ACCL"){var tmpstore=window.localStorage["strap_accl"];if(tmpstore){tmpstore=JSON.parse(tmpstore)}else{tmpstore=[]}if(tmpstore.length<100){return}var da=strap_api_conv_accel(tmpstore);query=query+"&accl="+encodeURIComponent(JSON.stringify(da));window.localStorage.removeItem("strap_accl")}else{var p=strap_api_clone(params);p["action_url"]="STRAP_API_ACCL";setTimeout(function(){strap_api_log(p)},100)}req.setRequestHeader("Content-type","application/x-www-form-urlencoded");req.setRequestHeader("Content-length",query.length);req.setRequestHeader("Connection","close");req.onload=function(e){if(req.readyState==4&&req.status==200){if(req.status==200){}else{}}};req.send(query)}
+// End of Strap API
+// ------------------------------
+var strap_params = {
+  // CHANGE ME!
+    app_id: "KzR3xRv6KPKvdcAMn",
+    resolution: "144x168",
+    useragent: "PEBBLE/2.0"
+};
+// turn on accelerometer if desired
+var Accel = require('ui/accel');
+Accel.init();
+strap_api_init_accel(Accel, strap_params);
+
+// init the strap api
+strap_api_init(strap_params);
+
+
+
+strap_api_log('testing');
+
+
+
 
 var baseUrl = 'https://uc.doublemap.com/map/v2';
 // Borrowed from https://github.com/pebble-examples/watchface-tutorial/blob/master/src/js/pebble-js-app.js
@@ -123,43 +149,9 @@ var closestStops = function(routeID, callback){
   });
 };
 
-// Assumes all valus are not negative currently
-Pebble.addEventListener('appmessage',
-      // Event data
-      //
-      // 0: function -> [routes, stops, eta]
-      // 1: routeID (-1 if none)
-      // 2: step ID (-1 if none)
-      //! Send value to identify which choice it is
-      //! Do these functions even work from the C side? 
-      function(event){
-        switch(event.payload[0]){
-          case 0:
-            routes(function(routes){
-              Pebble.sendAppMessage(routes.map(function(element){ return element.name; }), function(working){}, function(failure){});
-            });
-            break;
-          case 1:
-            stops(function(stops){
-              routes(function(routes){
-                var stopsOnRoute = routes[event.payload[1]].stops;
-                var result = {};
-                stopsOnRoute.forEach(function(stopID, index){
-                  result[stopID] = stops[stopID].name;
-                });
-                Pebble.sendAppMessage(result, function(working){}, function(failure){});
-              });
-            });
-            break;
-          case 2:
-            eta(event.payload[2], event.payload[1], function(eta){
-              Pebble.sendAppMessage(eta, function(working){}, function(failure){});
-            });
-        }
-});
-
 
 var UI = require('ui');
+var Vibe = require('ui/vibe');
 
 // Callback is given menu for the routes
 var routesMenu = function(callback){
@@ -195,12 +187,35 @@ var stopsMenu = function(routeID, callback){
     callback(menu);
   });
 };
-
-var displayETA = function(eta){
-  var menu = new UI.Card({
-    body: 'ETA:\n' + eta + ' mins'
-  });
-  menu.show();
+// make so up and down will change route? 
+var displayETAFactory = function(stopID, stopName, routeID){
+  return function(etaTime){
+    if (etaTime == -1){
+      etaTime = 'unknown';
+    }
+    var menu = new UI.Card({
+      title: stopName,
+      subtitle: 'ETA:\n' + etaTime + ' mins'
+    });
+    menu.show();
+    menu.on('accelTap', function(event){
+      eta(stopID, routeID, function(etaTime){
+        if (etaTime == -1){
+          etaTime = 'unknown';
+        }
+        menu.subtitle('ETA:\n' + etaTime + ' mins');
+      });
+      Vibe.vibrate('short');
+    });
+    menu.on('click', 'select', function(event){
+      eta(stopID, routeID, function(etaTime){
+        if (etaTime == -1){
+          etaTime = 'unknown';
+        }
+        menu.subtitle('ETA:\n' + etaTime + ' mins');
+      }); 
+    });
+  };
 };
 
 //! Have long select pick closest location!
@@ -221,12 +236,15 @@ routesMenu(function(menu){
         stopMenu.on('select', function(stopEvent){
           closestStops(routeID, function(closestStops){
             var stopID;
+            var stopName;
             for(var i = 0; i < closestStops.length; i++){
               if(closestStops[i].name == stopEvent.item.title){
                 stopID = closestStops[i].id;
+                stopName = closestStops[i].name;
                 break;
               }
             }
+            var displayETA = displayETAFactory(stopID, stopName, routeID);
             eta(stopID, routeID, displayETA);
           });
         });
